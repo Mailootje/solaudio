c87032b4c12146e5dec2d4928cccbb74a9ecc7f3
Mailo <oliam2002@hotmail.com>
2024-11-20 17:39:26 +0100

    Added Favicon & PWA

--- app/layout.server.tsx
+++ app/layout.server.tsx
@@ -1,6 +1,15 @@
 export const metadata = {
     title: 'SolAudio',
     description: 'Explore radio stations worldwide',
+    icons: {
+        icon: [
+            { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+            { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+            { rel: 'icon', href: '/favicon.ico' },
+        ],
+        apple: { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+        manifest: '/site.webmanifest',
+    },
 };
 
 export default function ServerLayout({ children }: { children: React.ReactNode }) {
